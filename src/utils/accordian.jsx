ecf100411f4f47684b2fa9d4d50d23300b974015
Alizba <elizbhaali7@gmail.com>
2026-01-29 18:19:24 +0500

    Install Packages and added data

---
+++ src/utils/accordian.jsx
@@ -0,0 +1,21 @@
+import { XCircle, BarChart3, ShieldCheck } from "lucide-react";
+
+const data = [
+    {
+        icon: <ShieldCheck/>,
+        heading : "Best Interest Rates in the Market",
+        details : "Exercitation in fugiat est ut as=d ea cupidatat ut in cupidatat occaecat ut occaecat"
+    },
+    {
+        icon: <XCircle/>,
+        heading : "Prevent Unstable Prices",
+        details : "Exercitation in fugiat est ut as=d ea cupidatat ut in cupidatat occaecat ut occaecat"
+    },
+    {
+        icon: <BarChart3/>,
+        heading : "Best Prices in the Market",
+        details : "Exercitation in fugiat est ut as=d ea cupidatat ut in cupidatat occaecat ut occaecat"
+    }
+]
+
+export default data
